da03bf283aa73445db9299403448c1b8cd6bff4b
askming <askming@gmail.com>
2014-03-19 10:15:02 -0500

    add folder 'reading of interest'

---
+++ C++ primer/C++ primer/C++ Primer/E1.1.1.cpp
@@ -0,0 +1,9 @@
+//
+//  E1.1.1.cpp
+//  C++ Primer
+//
+//  Created by Yang Ming on 3/15/14.
+//  Copyright (c) 2014 askming. All rights reserved.
+//
+
+#include "E1.1.1.h"
---
+++ C++ primer/C++ primer/C++ Primer/E1.1.1.h
@@ -0,0 +1,14 @@
+//
+//  E1.1.1.h
+//  C++ Primer
+//
+//  Created by Yang Ming on 3/15/14.
+//  Copyright (c) 2014 askming. All rights reserved.
+//
+
+#ifndef __C___Primer__E1_1_1__
+#define __C___Primer__E1_1_1__
+
+#include <iostream>
+
+#endif /* defined(__C___Primer__E1_1_1__) */
